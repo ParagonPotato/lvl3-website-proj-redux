65206224289169fc1aabb74b9ff01aa00f891f39
ParagonPotato <54691303+ParagonPotato@users.noreply.github.com>
2021-11-17 18:23:50 +1300

    added login and register phps

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //connect to database
+    $conn = mysqli_connect("localhost","root","","digital");
+
+    //querying
+    $result = mysqli_query($conn,"select * from users where username = '$username' and password = '$password'")
+                or die("Something hucked up ".mysql_error());
+    $row = mysqli_fetch_array($result);
+    if ($row['username'] == $username && $row['password'] == $password ){
+        echo "Login successful! Welcome ".$row['username'];
+    } else {
+        echo "Failed to find user";
+    }
+?>
---
+++ register.php
@@ -0,0 +1,22 @@
+<?php
+$serverName = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "digital";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$conn = mysqli_connect($serverName,$dbUsername,$dbPassword,$dbName);
+
+if (!$conn) {
+	die("Connection failed: " .mysqli_connect_error());
+} else {
+	$stmt = $conn->prepare("insert into users(username,password) values(?,?)");
+	$stmt->bind_param("ss",$username,$password);
+	$stmt->execute();
+	echo "registered successfully";
+	$stmt->close();
+	$conn->close();
+}
+?>
