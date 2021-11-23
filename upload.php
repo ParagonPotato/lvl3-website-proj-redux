adc12f8d38bfda9c0fe74369f32a4286c6abc4b7
ParagonPotato <54691303+ParagonPotato@users.noreply.github.com>
2021-11-23 15:47:07 +1300

    changed redirections

--- upload.php
+++ upload.php
@@ -16,7 +16,7 @@ $date = $_POST['date'];
 $category = $_POST['category'];
 $value = $_POST['value'];
 
-if(empty($name)) { header('Location: dropoff.php?lognameempty=t');exit(); }
+if(empty$name)) { header('Location: dropoff.php?lognameempty=t');exit(); }
 if(empty($date)) { header('Location: dropoff.php?logdateempty=t');exit(); }
 if(empty($category)) { header('Location: dropoff.php?logcategoryempty=t');exit(); }
 if(empty($value)) { header('Location: dropoff.php?logvalueempty=t');exit(); }
@@ -26,11 +26,11 @@ $conn = mysqli_connect($serverName,$dbUsername,$dbPassword,$dbName);
 if (!$conn) {
 	die("Connection failed: ".mysqli_connect_error());
 } else {
-	$stmt = $conn->prepare("insert into items(name,date,category,value) values(?,?,?,?)");
+	$stmt = $conn->prepare("insert into lostitems(name,date,category,value) values(?,?,?,?)");
 	$stmt->bind_param("ssss",$name,$date,$category,$value);
 	$stmt->execute();
 	$stmt->close();
 	$conn->close();
-    header('Location: dashboard.php?id='.$id.'regsuccess=t');
+    header('Location: dashboard.php?id='.$id.'&regsuccess=t');
 }
 ?>
