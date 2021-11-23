5996e5384ecabbfc1d4bdb65ca6229338d9755eb
ParagonPotato <54691303+ParagonPotato@users.noreply.github.com>
2021-11-24 00:39:06 +1300

    tried adding hashing to passwords. it is not working

--- login.php
+++ login.php
@@ -3,17 +3,18 @@
 
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $passwordhashed = password_hash($password,PASSWORD_DEFAULT);
 
     //connect to database
     $conn = mysqli_connect("localhost","root","","digital");
 
     //querying
-    $result = mysqli_query($conn,"select * from users where username = '$username' and password = '$password'")
+    $result = mysqli_query($conn,"select * from users where username = '$username' and password = '$passwordhashed'")
                 or die("Something hucked up ".mysql_error());
     $row = mysqli_fetch_array($result);
-    if ($row['username'] == $username && $row['password'] == $password ){
+    if ($row['username'] == $username && password_verify($password,$row['password']) == 1 ){
         echo "Login successful! Welcome ".$row['name'];
-        $_SESSION['id']=$row['id'];
+        $_SESSION['id']=$row['user_id'];
         header('Location: dashboard.php');
     } else {
         header('Location: index.php?loginerror=t');
