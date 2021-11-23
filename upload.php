affbb23d0a2309f1d9e9fc2d305fe63eb94807a6
ParagonPotato <54691303+ParagonPotato@users.noreply.github.com>
2021-11-24 12:50:04 +1300

    made it work

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION['id'])) {
     exit();
 }
 
-$id = $_GET['id'];
+$id = $_SESSION['id'];
 
 $serverName = "localhost";
 $dbUsername = "root";
@@ -14,21 +14,24 @@ $dbName = "digital";
 
 $name = $_POST['name'];
 $date = $_POST['date'];
+$location = $_POST['location'];
 $category = $_POST['category'];
 $value = $_POST['value'];
+$poster = $_POST[$id];
 
 if(empty($name)) { header('Location: dropoff.php?lognameempty=t');exit(); }
 if(empty($date)) { header('Location: dropoff.php?logdateempty=t');exit(); }
+if(empty($location)) { header('Location: dropoff.php?loglocationempty=t');exit(); }
 if(empty($category)) { header('Location: dropoff.php?logcategoryempty=t');exit(); }
 if(empty($value)) { header('Location: dropoff.php?logvalueempty=t');exit(); }
 
 $conn = mysqli_connect($serverName,$dbUsername,$dbPassword,$dbName);
-
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 if (!$conn) {
 	die("Connection failed: ".mysqli_connect_error());
 } else {
-	$stmt = $conn->prepare("INSERT INTO `lostitems` (`name`, `date`, `category`, `value`) VALUES ('Trilogy', current_timestamp(), 'Album', '50');");
-	$stmt->bind_param("ssss",$name,$date,$category,$value);
+	$stmt = $conn->prepare("INSERT INTO `lostitems` (`name`, `date`,`location`, `category`, `value`, `poster`) VALUES (?,?,?,?,?,?);");
+	$stmt->bind_param("ssssii",$name, $date, $location, $category, $value, $id);
 	$stmt->execute();
 	$stmt->close();
 	$conn->close();
