fed1874f9fdd9894fea34d4a5f0e14269b350ce6
ParagonPotato <54691303+ParagonPotato@users.noreply.github.com>
2021-12-05 20:42:08 +1300

    updating

--- login.php
+++ login.php
@@ -8,15 +8,17 @@
     //connect to database
     $conn = mysqli_connect("localhost","root","","digital");
 
+    if
+
     //querying
     $result = mysqli_query($conn,"select * from users where username = '$username'")
-                or die("Something hucked up ".mysql_error());
+                or die("Something hucked up ".mysqli_error($conn));
     $row = mysqli_fetch_array($result);
     if ($row['username'] == $username && password_verify($password,$row[2]) ){
         echo "Login successful! Welcome ".$row['name'];
         $_SESSION['id']=$row['user_id'];
         $_SESSION['admin']=$row['admin'];
-        header('Location: dashboard.php');
+    header('Location: dashboard.php');
     } else {
         echo password_verify($password,$row[2]);
         header('Location: index.php?loginerror=t');
